a9735b22a672b13f218bed0c3a84eef45e64546b
mitchdz <mitch_dz@hotmail.com>
2020-10-06 20:43:39 -0700

    hw3: find Hough Peaks
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- hw3/src/hw3.c
+++ hw3/src/hw3.c
@@ -13,10 +13,7 @@
 #include <float.h>
 #include <math.h>
 
-#define HOUGH_ROWS 100
-#define HOUGH_COLS 100
-
-uint8_t threshold_value = 110;
+uint8_t threshold_value = 225;
 
 int main(int argc,char* argv[]) {
     if (argc==1) {
@@ -29,28 +26,36 @@ exit:
     return 0;
 }
 
-void HTStraightLine(uint8_t **edge_map, int32_t n_rows, int32_t n_cols,
-        int32_t **HA);
+void writePNG(uint8_t** raw_data, char* filename, int n_rows, int n_cols)
+{
+    PNGFILE *output_png = pngOpen(filename, "w");
+    pngWriteHdr(output_png, n_rows, n_cols);
+    for (int r = 0; r < n_rows; r++) pngWriteRow(output_png, raw_data[r]);
+    free(output_png);
+}
 
 void analyzeImage()
 {
     int32_t c, r, n_cols, n_rows;
 
+    //initialize input picture
     PNGFILE *pngfile = pngOpen(INPUT_PICTURE, "r");
     pngReadHdr(pngfile, &n_rows, &n_cols);
     printf("image is %d rows by %d cols\n", n_rows, n_cols);
 
-    uint8_t **png_raw = matalloc(n_rows, n_cols, 0, 0, sizeof(uint8_t));
-    int32_t **HA      = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(int32_t));
-    uint8_t **HAout   = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(uint8_t));
+    //initial pseudo 2D arrays
+    uint8_t **png_raw   = matalloc(n_rows, n_cols, 0, 0, sizeof(uint8_t));
+    int32_t **HA        = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(int32_t));
+    uint8_t **HAout     = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(uint8_t));
+    uint8_t **HAlines   = matalloc(n_rows, n_cols, 0, 0, sizeof(uint8_t));
 
     //store entire image into memory
     for (r = 0; r < n_rows; r++) pngReadRow(pngfile, png_raw[r]);
 
+    // call HTStraightLine function
     HTStraightLine(png_raw, n_rows, n_cols, HA);
 
-
-    //scale HoughArray
+    //find max and min value in Hough Array
     double max = DBL_MIN, min = DBL_MAX;
     for (r = 0; r < HOUGH_ROWS; r++) {
         for (c  = 0; c < HOUGH_COLS; c++) {
@@ -59,28 +64,75 @@ void analyzeImage()
         }
     }
 
+    //properly scale hough array between 0 and 255
+    for (r = 0; r < HOUGH_ROWS; r++)
+        for (c  = 0; c < HOUGH_COLS; c++)
+            HAout[r][c] = ((HA[r][c]-min)/(max-min))*255;
+
+    // write grayscale to file
+    writePNG(HAout, OUTPUT_HOUGH_ARRAY, HOUGH_ROWS, HOUGH_COLS);
+
+    // NMS to detect Hough Peaks
+    for (r = 0; r < HOUGH_ROWS; r++)
+        for (c  = 0; c < HOUGH_COLS; c++)
+            HAout[r][c] = (HAout[r][c] < threshold_value) ? 0 : HAout[r][c];
+
+    // write detected Hough Peaks to file
+    writePNG(HAout, OUTPUT_HOUGH_ARRAY, HOUGH_ROWS, HOUGH_COLS);
+
+    // Convert Hough Peaks in rho/theta to lines in cartesian
+    for (r = 0; r < n_rows; r++)
+        for (c  = 0; c < n_cols; c++)
+            HAlines[r][c] = 0;
+
+    // create head of singly linked list
+    HoughPeakNode* head = (HoughPeakNode*)malloc(sizeof(HoughPeakNode));
+    // We can compute the line in the image space by using
+    // y =           m           * x +      b
+    // y = -cos(theta)/sin(theta)* x + p/sin(theta)
+    double m, b, rho, theta;
     for (r = 0; r < HOUGH_ROWS; r++) {
         for (c  = 0; c < HOUGH_COLS; c++) {
-            HAout[r][c] = ((HA[r][c]-min)/(max-min))*255;
+            if (HAout[r][c] >= threshold_value) {
+                rho = r;
+                theta = c;
+
+                m = (-cos(theta)/sin(theta));
+                b = rho/sin(theta);
+                HoughPeakNode* node = \
+                                (HoughPeakNode*)malloc(sizeof(HoughPeakNode));
+                node->theta = theta;
+                node->rho = rho;
+                node->m = m;
+                node->b = b;
+                node->next = NULL;
+
+                appendHoughPeakNode(head, node);
+            }
         }
     }
 
+    //TODO: write lines on HALines
 
-    //TODO: NMS (Non-Maxima Supression)
 
-    // write grayscale to file
-    PNGFILE *output_png = pngOpen(OUTPUT_PICTURE, "w");
-    pngWriteHdr(output_png, HOUGH_ROWS, HOUGH_COLS);
-    for (r = 0; r < HOUGH_ROWS; r++) pngWriteRow(output_png, HAout[r]);
+
+    printf("There are %d Hough Peaks with a threshold of %d\n",
+            getNumberHoughPeakNodes(head), threshold_value);
 
     goto cleanup;
 cleanup:
+    freeHoughPeakNodeList(head);
     matfree(png_raw);
     matfree(HA);
+    matfree(HAout);
+    matfree(HAlines);
     pngClose(pngfile);
-    pngClose(output_png);
     return;
 }
+
+
+
+
 
 /* @brief performs Hough Transform using rho/theta parameterization on an
  *      edge map and returns the accumulator
@@ -89,7 +141,7 @@ cleanup:
  *      on
  * @param n_rows uint8_t argument to define picture rows
  * @param n_cols uint8_t argument to define picture cols
- * @param HA double pointer to psuedo-2D array created by matalloc
+ * @param HA double pointer to psuedo-2D int32_t array created by matalloc
  * @return void
  * @note writes values into HA
  */
@@ -115,18 +167,19 @@ void HTStraightLine(uint8_t **edge_map, int32_t n_rows, int32_t n_cols,
     int HA_rho_min = n_cols;
     int HA_rho_max = (int)sqrt(pow(n_rows,2)+pow(n_cols,2));
 
-    printf("HA_rho_min: %d HA_rho_max: %d\n", HA_rho_min, HA_rho_max);
-
     int32_t r, c;
     int8_t HA_theta, HA_rho;
     double theta, p;
     bool valid_edge;
+    //pragma omp parallelizes the OUTER 2 loops w/ collapse(2) argument
     #pragma omp parallel for private(r, c, p, theta) shared(HA) collapse(2)
     for (r = 0; r < n_rows; r++) {
         for (c = 0; c < n_cols; c++) {
+            //valid_edge checks if the pixel is an edge or not
             valid_edge = (edge_map[r][c] > 100) ? true : false;
             if (valid_edge) {
                 for (theta = 0; theta < M_PI; theta += M_PI/99) {
+                    //classic rho algorithm
                     p = r*cos(theta) + c*sin(theta);
 
                     // theta is between 0 and 99 in Hough Array
@@ -151,4 +204,3 @@ void HTStraightLine(uint8_t **edge_map, int32_t n_rows, int32_t n_cols,
     }
     return;
 }
-
--- hw3/src/hw3.h
+++ hw3/src/hw3.h
@@ -2,7 +2,55 @@
 #include <stdint.h>
 #include "pngio.h"
 
+#define HOUGH_ROWS 100
+#define HOUGH_COLS 100
+
 #define INPUT_PICTURE "input/edges.png"
-#define OUTPUT_PICTURE "out/out.png"
+#define OUTPUT_PICTURE "out/HoughPeaks.png"
+#define OUTPUT_HOUGH_ARRAY "out/HoughArray.png"
+#define OUTPUT_HOUGH_LINES "out/HoughLines.png"
+
+typedef struct HoughPeakNode {
+    double theta;
+    double rho;
+    double m;
+    double b;
+
+    struct HoughPeakNode* next;
+} HoughPeakNode;
+
+void freeHoughPeakNodeList(HoughPeakNode* head)
+{
+    HoughPeakNode* tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+
+}
+
+void appendHoughPeakNode(HoughPeakNode* head, HoughPeakNode* new)
+{
+    HoughPeakNode* tmpNode = head;
+    while (tmpNode->next != NULL) {
+        tmpNode = tmpNode->next;
+    }
+    tmpNode->next = new;
+}
+
+int  getNumberHoughPeakNodes(HoughPeakNode* head)
+{
+    int count = -1;
+    HoughPeakNode* tmpNode = head;
+    while (tmpNode != NULL) {
+        tmpNode = tmpNode->next;
+        count++;
+    }
+    return count;
+}
 
 void analyzeImage();
+void HTStraightLine(uint8_t **edge_map, int32_t n_rows, int32_t n_cols,
+        int32_t **HA);
+void writePNG(uint8_t** raw_data, char* filename, int n_rows, int n_cols);
