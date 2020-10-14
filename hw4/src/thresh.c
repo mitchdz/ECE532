958cf10c7a6069f9a49f3dc0b900273ffc385963
mitchdz <mitch_dz@hotmail.com>
2020-10-14 15:14:27 -0700

    hw4: add all Recursive helper functions for Kittler Illingworth formula
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- hw4/src/thresh.c
+++ hw4/src/thresh.c
@@ -13,252 +13,229 @@
 #include <float.h>
 #include <math.h>
 
-#define HOUGH_ROWS 100
-#define HOUGH_COLS 100
-
 int8_t threshold_value = -1; // user defined or Kittler's method
 
 char *INPUT_FILE = "";
 char *OUTPUT_FILE = "";
 
-
-void printHelp()
-{
-    //TODO: print help menu
-}
-
-/*
- * call the program as
- * thresh [-t T] infile outfile
- *
- * T: optional user specified threshold 0 <= T <= 255;
- *  default: T is automatically computed using Kittler's method
- *
- * infile: the input grayscale image
- *
- * outfile: the output bi-level image
- */
 int main(int argc,char* argv[]) {
-    if (argc==1) { //just calling the program by itself
-        printf("Error: provide correct amount of inputs");
-        printHelp();
-    }
-    if (argc==2) { // calling program with a single param
-        printf("Error: provide correct amount of inputs");
-        printHelp();
-    }
+    error_t error;
     if (argc==3) { // calling program with 2 params
         INPUT_FILE = argv[2]; //TODO: error check
         OUTPUT_FILE = argv[3]; //TODO: error check
-        analyzeImage();
+        printf("input file: %s\n", INPUT_FILE);
+        printf("output file: %s\n", OUTPUT_FILE);
+        printf("threshold: not defined\n");
+        error = analyzeImage();
     }
-    if (argc==4) { //not enough params
-        printf("Error: provide correct amount of inputs");
-        printHelp();
-    }
-    if (argc==5) { // defining threshold
+    else if (argc==5) { // defining threshold
         threshold_value = atoi(argv[2]); //TODO: error check
-        INPUT_FILE = argv[3]; //TODO: error check
-        OUTPUT_FILE = argv[4]; //TODO: error check
-        analyzeImage();
+        INPUT_FILE = argv[2]; //TODO: error check
+        OUTPUT_FILE = argv[3]; //TODO: error check
+        printf("input file: %s\n", INPUT_FILE);
+        printf("output file: %s\n", OUTPUT_FILE);
+        printf("threshold: %d\n", threshold_value);
+        error = analyzeImage();
     }
-    if (argc>5) { //too many params
+    else { //too many params
         printf("Error: provide correct amount of inputs");
         printHelp();
+        goto exit;
     }
+
 exit:
     return 0;
 }
 
-typedef enum {
-    UINT8_T,
-    INT32_T,
-} TYPE;
-
-
-//_hw4_error zeroPsuedo2DArray(void** array, int32_t n_rows, int32_t n_cols, \
-//        TYPE type)
-//{
-//    int32_t r, c;
-//    //zero out Hough Array
-//    for (r = 0; r < n_rows; r++) {
-//        for (c = 0; c < n_cols; c++) {
-//            switch(type) {
-//                case INT32_T:
-//                    ((int32_t**)array)[r][c] = 0;
-//                    break;
-//                case UINT8_T:
-//                    ((uint8_t **)array)[r][c] = 0;
-//                    break;
-//            }
-//        }
-//    }
-//    return E_SUCCESS;
-//}
-
-double P(uint8_t *h, int t)
-{
-    // P(i) = h[i]/#pixels
-    return  h[t] / (sizeof(*h) / sizeof(h[0]));
-}
-
-double q1(uint8_t *h, int t)
-{
-    double sum = 0;
-    for (int i = 0; i <= t; i++)
-        sum += P(h, t);
-    return sum;
-}
-
-double q2(uint8_t *h, int t) { return (double)1 - q1(h, t); }
-
-double u1(uint8_t *h, int t)
-{
-    //      t
-    // u1 = Σ iP(i)/q1(t)
-    //      i=0
-    double sum = 0;
-    for (int i = 0; i <= t; i++)
-        sum += i*(P(h,t))/q1(h,t);
-    return sum;
-}
-
-double u2(uint8_t *h, int t)
-{
-    //      255
-    // u2 = Σ i*P(i)/q2(t)
-    //      i=t+1
-    double sum = 0;
-    for (int i = t + 1; i <= 255; i++)
-        sum += i*(P(h,t))/q2(h,t);
-    return sum;
-}
-
-
-double var1(uint8_t *h, int t)
-{
-    //        t
-    // var1 = Σ ([i-u1(t)]^2 * P(i) * q1(t))
-    //        i=0
-    double sum = 0;
-    for (int i = 0; i <= t; i++)
-        sum += pow(i-u1(h,t),2)*(P(h,t))/q1(h,t);
-    return sum;
-}
-
-double var2(uint8_t *h, int t)
-{
-    //        255
-    // var2 = Σ ([i-u2(t)]^2 * P(i) * q2(t))
-    //        i=t+1
-    double sum = 0;
-    for (int i = t + 1; i <= 255; i++)
-        sum += pow(i-u2(h,t),2)*(P(h,t))/q2(h,t);
-    return sum;
-}
-
-double f(uint8_t *h, int t)
-{
-    // f(t) = q1*gauss1 + q2*gauss2
-    double g1 = q1(h,t)/( var1(h,t)*sqrt(2*M_PI) ) *
-        pow( M_E, -1.0 * pow( (t-u1(h,t)),2)/(2*var1(h,t) ) );
-
-    double g2 = q2(h,t)/( var2(h,t)*sqrt(2*M_PI) ) *
-        pow( M_E, -1.0 * pow( (t-u2(h,t)),2)/(2*var2(h,t) ) );
-
-    return g1 + g2;
-
-}
-
-uint8_t * convert2DPseudoArrayToHistogram(uint8_t **grayscale, int32_t n_rows,
-        int32_t n_cols)
-{
-    uint8_t Histogram[n_rows * n_cols];
-    int i, j, k =0 ;
-    for (i=0;i<n_rows;i++) {
-        for (j=0;j<n_cols;j++) {
-            Histogram[k] = grayscale[i][j];
-            k++;
-        }
-    }
-    return Histogram;
-}
-
-
-// Kittler and Illingworth's Kullback information minimization approach.
-int32_t KittlerIllingworthThresholding(uint8_t **grayscale, int32_t n_rows,
-        int32_t n_cols)
-{
-    double q1, q2, u1, u2, var1, var2;
-    int32_t T = -1;
-    int i, j;
-
-    // step 1) compute histogram, h(i)
-    // convert grayscale to histogram
-    uint8_t *Histogram;
-    Histogram = convert2DPseudoArrayToHistogram(grayscale, n_rows, n_cols);
-
-    // Step 2) Normalize: P(i) = h(i)/#pixels
-    // done in function above (:
-
-    // step 3) for each t, compute H(t)
-    double min = -1, H, sum;
-    for (int t = 0; t <= 255; t++) {
-        sum = 0;
-        for (i = 0; i < 255;i++) {
-            sum += -(P(Histogram,t)*log(f(Histogram,t)));
-        }
-        if ( H < min) T  = t;
-    }
-
-    // step 4) return t giving the smalled H(t)
-    return T;
-}
-
-double RecursiveUpdateFormula(double q1, double q2, double mu1prev,
-        double mu2prev, double *P, double q1prev, double var1prev, int32_t t,
-        double mu, double q2prev, double var2prev)
-{
-    double mu1, var1, mu2, var2, H;
-    mu1 = (q1prev*mu1prev+t*P[t])/q1;
-
-    var1 = (q1prev*(var1prev+(mu1prev-mu1)*(mu1prev-mu1)) +P[t]*(t-mu1)*(t-mu1))
-        /q1;
-
-    mu2 = (mu-q1*mu1)/q2;
-
-    var2 = (q2prev*(var2prev+(mu2prev-mu2)*(mu2prev-mu2)) -P[t]*(t-mu2)*(t-mu2))
-        /q2;
-
-    H = (q1*log(var1)+q2*log(var2))/2-q1*log(q1)-q2*log(q2);
-
-    return H;
-}
-
-
-void analyzeImage()
+error_t analyzeImage()
 {
     int32_t c, r, n_cols, n_rows;
-    //initialize input picture
+    error_t error;
+    // initialize input picture
     PNGFILE *pngfile = pngOpen(INPUT_FILE, "r");
     pngReadHdr(pngfile, &n_rows, &n_cols);
     printf("image is %d rows by %d cols\n", n_rows, n_cols);
 
-    //initial pseudo 2D arrays
-    uint8_t **png_raw = matalloc(n_rows, n_cols, 0, 0, sizeof(uint8_t));
-    int32_t **HA      = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(int32_t));
-    uint8_t **HAout   = matalloc(HOUGH_ROWS, HOUGH_COLS, 0, 0, sizeof(uint8_t));
+    // store entire image into memory
+    uint8_t **grayscale = matalloc(n_rows, n_cols, 0, 0, sizeof(uint8_t));
+    for (r = 0; r < n_rows; r++) pngReadRow(pngfile, grayscale[r]);
 
-    //store entire image into memory
-    for (r = 0; r < n_rows; r++) pngReadRow(pngfile, png_raw[r]);
+    int8_t x[n_rows*n_cols];
 
-    //zeroPsuedo2DArray(HA, HOUGH_ROWS, HOUGH_COLS, INT32_T);
+    // convert image to histogram
+    uint8_t *Histogram;
+    error = convert2DPseudoArrayToHistogram(grayscale, n_rows, n_cols,
+            Histogram);
+    if (error != E_SUCCESS) {
+        printError(error, "Could not convert picture to histogram");
+        goto cleanup;
+    }
 
+    // if no threshold defined, determine threshold
+    if (threshold_value == -1) {
+        error=KittlerIllingworthThresholding(Histogram, &threshold_value);
+        if (error != E_SUCCESS) {
+            printError(error, "Error when trying to find thresholding value");
+            goto cleanup;
+        }
+    }
 
-    goto cleanup;
+    // Recursive update formula to find x
+    error=RecursiveUpdateFormula(Histogram, threshold_value);
+    if (error != E_SUCCESS) {
+        printError(error, "Could not execute recursive update formula");
+        goto cleanup;
+    }
+
+    // convert resulting array to grayscale
+    error=convertHistogramToPseudo2DArray(Histogram, n_rows,n_cols, grayscale);
+    if (error != E_SUCCESS) {
+        printError(error, "Could not convert histogram to pseudo 2D array");
+        goto cleanup;
+    }
+    for (int r = 0; r < n_rows; r++) {
+        for (int c = 0; c < n_cols; c++) {
+        grayscale[r][c] = (grayscale[r][c] >= threshold_value) ? 255 : 0;
+        }
+    }
+
+    //write output grayscale to file
+    writePNG(grayscale, OUTPUT_FILE, n_rows, n_cols);
+
+    error = E_SUCCESS;
 cleanup:
-    matfree(png_raw);
-    matfree(HA);
-    matfree(HAout);
-    pngClose(pngfile);
+    if (grayscale != NULL) matfree(grayscale);
+    if (pngfile != NULL) pngClose(pngfile);
+    return error;
 }
+
+
+error_t KittlerIllingworthThresholding(uint8_t *histogram, int8_t *thresh)
+{
+    // step 1) compute histogram, h(i)
+    // converted in main (:
+
+    // Step 2) Normalize: P(i) = h(i)/#pixels
+    // done in helper function (:
+
+    // step 3) for each t, compute H(t) where H(t) is defined as follows:
+    //           t
+    //  H(t) = - Σ P(h,t)log(f(h,t))
+    //          i=0
+    int32_t T = -1, i, j;
+    double min = -1, sum;
+    for (int t = 0; t <= 255; t++) {
+        sum = 0;
+        for (i = 0; i < 255;i++)
+            sum += (KittlerP(histogram,t)*log(Kittlerf(histogram,t)));
+        if ( (-1.0 * sum) < min) T  = t;
+    }
+
+    // step 4) return t giving the smalled H(t)
+    *thresh = T;
+    return E_SUCCESS;
+}
+
+
+double Rq1(uint8_t *h, int8_t t)
+{
+    //q1(0) = P(0)
+    if ( t == 0 ) return KittlerP(h, 0);
+
+    //q1(t) = q1(t-1) + P(t)
+    return Rq1(h, t-1) + KittlerP(h,t);
+}
+
+double Rq2(uint8_t *h, int8_t t)
+{
+    //q2(t) = 1 - q1(t)
+    return 1 - Rq1(h,t);
+}
+
+double Ru1(uint8_t *h, int8_t t)
+{
+    // u1(0) = 0
+    if ( t == 0 ) return 0;
+
+    // u1(t) = q1(t-1)*u1(t-1) + t*P(t) / q1(t)
+    return (Rq1(h,t-1)*Ru1(h,t-1) + t*KittlerP(h,t))/Rq1(h,t);
+}
+
+//      255
+//  u =  Σ i*P(h,t)
+//      i=0
+double Ru(uint8_t *h)
+{
+    double sum = 0;
+    for (int i = 0; i <= 255; i++)
+        sum += i*KittlerP(h,i);
+    return sum;
+}
+
+double Ru2(uint8_t *h, int8_t t)
+{
+    // u2(0) = u/q2(0)
+    if (t == 0) return (Ru(h)/Rq2(h,0));
+
+    //u2(t) = (u-q1(t)u1(t))/q2(t)
+    return (Ru(h)-Rq1(h,t)*Ru1(h,t))/Rq2(h,t);
+}
+
+double Rvar1(uint8_t *h, int8_t t)
+{
+    //var1(0) = 0
+    if ( t == 0) return 0;
+
+    //var1(t) = a lot of stuff
+    return (1.0/Rq1(h,t)) * ( (Rq1(h,t-1) \
+            * ( Rvar1(h,t-1) + pow(Ru1(h,t-1)-Ru1(h,t),2)) \
+            + KittlerP(h,t) * pow((t-Ru1(h,t)),2)) );
+}
+
+double Rvar2(uint8_t *h, int8_t t)
+{
+    int i;
+    double sum;
+    //         255
+    //  var2(0) = Σ [i - u2(0)]^2 * P(i)/q2(0)
+    //         i=1
+    if ( t == 0 ) {
+        sum = 0;
+        for (i = 1; i <= 255; i++) {
+            sum += (pow(i-Ru2(h,0),2) * KittlerP(h,t)/Rq2(h,0));
+        }
+        return sum;
+    }
+
+    // var2(t) = a lot of annoying stuff to type out
+    return (1.0/Rq2(h,t)) * ( (Rq2(h,t-1) \
+            * ( Rvar2(h,t-1) + pow(Ru2(h,t-1)-Ru2(h,t),2)) \
+            + KittlerP(h,t) * pow((t-Ru2(h,t)),2)) );
+}
+
+error_t RecursiveUpdateFormula(uint8_t *h, int32_t t)
+{
+    double mu1, mu2, q1, q2, var1, var2, H;
+    double H;
+
+    double q1prev   =   q1(h,t-1);
+    double mu1prev  =   u1(h,t-1);
+    double var1prev = var1(h,t-1);
+
+
+    mu1 = ( q1prev*mu1prev + t*P(h,t) ) / q1(h,t);
+
+    var1 = (q1prev*(var1prev+(mu1prev-mu1)*(mu1prev-mu1)) +P[t]*(t-mu1)*(t-mu1))
+        / q1;
+
+    mu2 = (mu-q1*mu1)/q2;
+
+    var2 = (q2prev*(var2prev+(mu2prev-mu2)*(mu2prev-mu2)) -P[t]*(t-mu2)*(t-mu2))
+        / q2;
+
+    H = (q1*log(var1)+q2*log(var2))/2-q1*log(q1)-q2*log(q2);
+
+    return E_SUCCESS;
+}
+
+
--- hw4/src/thresh.h
+++ hw4/src/thresh.h
@@ -3,16 +3,20 @@
 #include <math.h>
 #include "pngio.h"
 
-#define INPUT_PICTURE "input/address.png"
-#define INPUT_PICTURE_2 "input/graybook.png"
-#define OUTPUT_THRESHOLD_FILENAME = "out/threshold.png"
+typedef enum {
+    UINT8_T,
+    INT32_T,
+} TYPE;
 
 /* hw4 error codes for debugging the program */
-typedef enum _hw4_error
+enum _hw4_error
 {
-    E_SUCCESS = 0,
-    E_NO_NODES = -1,
-} _hw4_error;
+    E_SUCCESS,
+    E_FILE_NOT_FOUND,
+    E_GENERIC_ERROR,
+};
+
+typedef enum _hw4_error error_t;
 
 /* helper function to easily decode error message */
 struct _errordesc{
@@ -20,7 +24,244 @@ struct _errordesc{
     char *message;
 } errordesc[] = {
     { E_SUCCESS,  (char *)"No error" },
-    { E_NO_NODES, (char *)"No nodes in linked list" },
+    { E_FILE_NOT_FOUND, (char *)"File not found" },
+    { E_GENERIC_ERROR,  (char *)"Generic Error" },
 };
 
-void analyzeImage();
+void printError(error_t E, char *msg)
+{
+    printf("Error %d: %s %s\n", errordesc[E].code, errordesc[E].message ,msg);
+}
+
+void printHelp()
+{
+    printf("call the program as\n");
+    printf("thresh [-t T] infile outfile\n");
+    printf("\n");
+    printf("T: optional user specified threshold 0 <= T <= 255;\n");
+    printf(" default: T is automatically computed using Kittler's method\n");
+    printf("\n");
+    printf("infile: the input grayscale image\n");
+    printf("\n");
+    printf("outfile: the output bi-level image\n");
+}
+
+error_t analyzeImage();
+error_t zeroPsuedo2DArray(void** array, int32_t n_rows, int32_t n_cols,
+        TYPE type);
+
+/* @brief P is the observed "probability distribution"
+ *        P = h(t)/#numpixels
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double KittlerP(uint8_t *h, int t) {return  h[t] / (sizeof(*h) / sizeof(h[0]));}
+
+/* @brief Kittler q1
+ *       t
+ *  q1 = Σ P(h,t)
+ *      i=0
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittlerq1(uint8_t *h, int t)
+{
+    double sum = 0;
+    for (int i = 0; i <= t; i++) sum += KittlerP(h, t);
+    return sum;
+}
+
+/* @brief Kittler q2
+ *       255
+ *  q2 = Σ P(h,t) = 1 - q1(h,t)
+ *      i=t+1
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittlerq2(uint8_t *h, int t) { return 1.0 - Kittlerq1(h, t); }
+
+/* @brief Kittler u1
+ *       t
+ *  u1 = Σ i*P(h,t)/q1(h,t)
+ *      i=0
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittleru1(uint8_t *h, int t)
+{
+    double sum = 0;
+    for (int i = 0; i <= t; i++) sum += i*(KittlerP(h,t))/Kittlerq1(h,t);
+    return sum;
+}
+
+/* @brief Kittler u2
+ *      255
+ *  u2 = Σ i*P(h,t)/q2(h,t)
+ *     i=t+1
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittleru2(uint8_t *h, int t)
+{
+    double sum = 0;
+    for (int i = t + 1; i <= 255; i++) sum += i*(KittlerP(h,t))/Kittlerq2(h,t);
+    return sum;
+}
+
+/* @brief Kittler var1
+ *       t
+ *  var1 = Σ ([i-u1(h,t)]^2 * P(h,t) * q1(h,t))
+ *      i=0
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittlervar1(uint8_t *h, int t)
+{
+    double sum = 0;
+    for (int i = 0; i <= t; i++)
+        sum += pow(i-Kittleru1(h,t),2)*(KittlerP(h,t))/Kittlerq1(h,t);
+    return sum;
+}
+
+/* @brief Kittler var2
+ *      255
+ *  var2 = Σ ([i-u2(h,t)]^2 * P(h,t) * q2(h,t))
+ *     i=t+1
+ * @param h histogram created as a single uint8_t array
+ * @param t index of histogram
+ * @return double
+ */
+double Kittlervar2(uint8_t *h, int t)
+{
+    double sum = 0;
+    for (int i = t + 1; i <= 255; i++)
+        sum += pow(i-Kittleru2(h,t),2)*(KittlerP(h,t))/Kittlerq2(h,t);
+    return sum;
+}
+
+/* @brief f is representation of histogram as sum of 2 gaussian functions
+ *   f(t) = q1*gauss1 + q2*gauss2
+ *
+ * @param h histogram as a pointer to uint8_t array
+ * @param t index of histogram gaussian representation
+ *
+ * @return double
+ */
+double Kittlerf(uint8_t *h, int t)
+{
+    double g1 = Kittlerq1(h,t)/( Kittlervar1(h,t)*sqrt(2*M_PI) ) *
+        pow( M_E, -1.0 * pow( (t-Kittleru1(h,t)),2)/(2*Kittlervar1(h,t) ) );
+
+    double g2 = Kittlerq2(h,t)/( Kittlervar2(h,t)*sqrt(2*M_PI) ) *
+        pow( M_E, -1.0 * pow( (t-Kittleru2(h,t)),2)/(2*Kittlervar2(h,t) ) );
+
+    return g1 + g2;
+
+}
+
+/* @brief converts Pseudo 2D array to 1D array
+ * @param grayscale pseudo 2D array created by matalloc
+ * @param n_rows number of rows of the pseudo 2D array
+ * @param n_cols number of columns of the pseudo 2D array
+ * @param Histogram address that the 1D array should be stored to
+ *
+ * @see matalloc
+ * @return error_t
+ */
+error_t convert2DPseudoArrayToHistogram(uint8_t **grayscale, int32_t n_rows,
+        int32_t n_cols, uint8_t *Histogram)
+{
+    uint8_t histogram[n_rows * n_cols];
+    int i, j, k =0 ;
+    for (i=0;i<n_rows;i++) {
+        for (j=0;j<n_cols;j++) {
+            histogram[k] = grayscale[i][j];
+            k++;
+        }
+    }
+    Histogram = histogram;
+    return E_SUCCESS;
+}
+
+convertHistogramToPseudo2DArray(uint8_t *Histogram, in32_t n_rows,
+        int32_t n_cols, uint8_t **out)
+{
+
+
+
+
+}
+
+
+
+
+/* @brief Kittler and Illingworth method to determine optimal threshold value
+ *
+ * @param histogram as pointer to a uint8_t array
+ * @param thresh pointer to int8_t that threshold value should be stored into
+ *
+ * @return error_t
+ */
+error_t KittlerIllingworthThresholding(uint8_t *histogram, int8_t *thresh);
+
+
+/* @brief Recursive Update Formula, this is the formula that determines
+ * each value in the histogram based on a threshold
+ *
+ * @param h histogram as a pointer to a uint8_t array
+ * @param t int32_t threshold value
+ *
+ * @return error_t
+ */
+error_t RecursiveUpdateFormula(uint8_t *h, int32_t t);
+
+
+/* @brief zeros a psuedo 2D array created by matalloc
+ *
+ * @param array pseudo 2D array
+ * @param n_rows number of rows of the array
+ * @param n_cols number of columns of the array
+ * @param type what type the array is using the enum TYPE
+ *
+ * @see matalloc
+ * @return error_t
+ */
+error_t zeroPsuedo2DArray(void** array, int32_t n_rows, int32_t n_cols,
+        TYPE type)
+{
+    int32_t r, c;
+    //zero out Hough Array
+    for (r = 0; r < n_rows; r++) {
+        for (c = 0; c < n_cols; c++) {
+            switch(type) {
+                case INT32_T:
+                    ((int32_t**)array)[r][c] = 0;
+                    break;
+                case UINT8_T:
+                    ((uint8_t **)array)[r][c] = 0;
+                    break;
+            }
+        }
+    }
+    return E_SUCCESS;
+}
+
+/* @brief writePNG writes a png to a file
+ * @param raw_data uint8_t pseudo 2-D array created by matalloc
+ * @param n_rows number of rows of the pseudo 2D array
+ * @param n_cols number of columns of the pseudo 2D array
+ */
+void writePNG(uint8_t** raw_data, char* filename, int n_rows, int n_cols)
+{
+    PNGFILE *output_png = pngOpen(filename, (char *)"w");
+    pngWriteHdr(output_png, n_rows, n_cols);
+    for (int r = 0; r < n_rows; r++) pngWriteRow(output_png, raw_data[r]);
+    free(output_png);
+}
+
